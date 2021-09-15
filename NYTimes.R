44479dd2817eb63bd305e4d826eb6af5e7869405
Zhibao-Li <87750207+Zhibao-Li@users.noreply.github.com>
2021-09-14 21:41:26 -0400

    Add files via upload

---
+++ NYTimes.R
@@ -0,0 +1,29 @@
+
+library(httr)
+library(jsonlite)
+library(urltools)
+
+
+for (i in 1:30){
+origin<-'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=salary&api-key=bNZo0nAD5O1L1B4tRhGNJZ7xAnT05aST'
+url<-param_set(origin,key='page',value=i)
+res<-GET(url)
+js<-rawToChar(res$content)
+data<-fromJSON(js)
+
+keyword<-data$response$docs$keywords
+web_url<-data$response$docs$web_url
+pub_date<-data$response$docs$pub_date
+
+Top_keywords=c()
+for (i in c(1:10)){
+  words<-data.frame(keyword[i])$value[1]
+  Top_keywords<-append(Top_keywords,words)
+}
+
+row<-data.frame(web_url,pub_date,Top_keywords,stringsAsFactors = FALSE)
+if (i==1){write.append(row,'Wage_Articles_NewYorkTimes.csv',append = TRUE,sep = ",",col.names=TRUE)}
+else{write.table(row,'Wage_Articles_NewYorkTimes.csv',append = TRUE,sep = ",",col.names=FALSE)}
+}
+
+
